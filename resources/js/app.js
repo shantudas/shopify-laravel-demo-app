f729f62ee7f1d03f5471189609dabfcb2102b699
shantudas <shantuchandradas@yahoo.com>
2024-11-13 17:17:41 +0600

    tailwind css implemented

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue';
-import router from './router/index.js';
+// import { createApp } from 'vue';
+// import App from './components/App.vue';
+// import router from './router/index.js';
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+// const app = createApp(App);
+// app.use(router);
+// app.mount('#app');
 
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+// import laravel from 'laravel-vite-plugin';
+//
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         vue(),
+//     ],
+// });
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
+
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-    ],
+    // plugins: [vue()],
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
+    },
 });
